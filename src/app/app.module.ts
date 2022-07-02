9d4d9f1e2145304a8db8edbdaa267143d9265005
wackynicky <nikhilsekhar4626@gmail.com>
2022-07-02 15:20:04 +0530

    Employee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddemployeeComponent
   ],
   imports: [
     BrowserModule,
